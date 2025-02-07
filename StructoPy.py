de78d4b252aac3790a223cb75f7653278e8435cd
Yuri Hakobyan <157562742+hakobyanyuri@users.noreply.github.com>
2025-02-07 20:00:22 +0400

    Add files via upload

---
+++ StructoPy.py
@@ -0,0 +1,61 @@
+class Person:
+    """
+    A class to represent a person with a name and age.
+    Supports comparison based on age.
+    """
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+    def __str__(self) -> str:
+        return f'Person {self.name}, {self.age} years old'
+
+    # Comparison methods based on age
+    def __ne__(self, other) -> bool:
+        return self.age != other.age
+
+    def __lt__(self, other) -> bool:
+        return self.age < other.age
+
+    def __le__(self, other) -> bool:
+        return self.age <= other.age
+
+    def __gt__(self, other) -> bool:
+        return self.age > other.age
+
+    def __ge__(self, other) -> bool:
+        return self.age >= other.age
+
+# Creating Person instances
+p1 = Person('Anna', 16)
+p2 = Person('Yuri', 14)
+p3 = Person('Hakob', 54)
+p4 = Person('Liana', 47)
+p5 = Person('Arina', 23)
+
+class Vector:
+    """
+    A class to represent a 3D vector.
+    Supports vector addition.
+    """
+    def __init__(self, x, y, z) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        """Returns a new Vector as the sum of two vectors."""
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __str__(self) -> str:
+        return f'Vector ({self.x}, {self.y}, {self.z})'
+
+# Creating Vector instances
+v1 = Vector(1, 2, 3)
+v2 = Vector(2, 4, 7)
+
+# Adding two vectors
+res = v1 + v2
+
+# Displaying the result
+print(res)
